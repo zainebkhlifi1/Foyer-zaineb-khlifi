19778501c170974276ecdea53522f4fc465175f6
zainebkhlifi1 <zaineb.khlifi@esprit.tn>
2023-11-08 16:34:17 +0100

    first commit

---
+++ src/main/java/tn/esprit/foyerzainebkhlifi/FoyerZainebKhlifiApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.foyerzainebkhlifi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoyerZainebKhlifiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoyerZainebKhlifiApplication.class, args);
+	}
+
+}
