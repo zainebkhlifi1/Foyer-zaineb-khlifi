9af4f3c7f207423521d603353dde9dedd80bda82
zainebkhlifi1 <zaineb.khlifi@esprit.tn>
2023-11-12 16:12:50 +0100

    first commit

--- src/main/java/tn/esprit/foyerzainebkhlifi/DAO/Entities/Reservation.java
+++ src/main/java/tn/esprit/foyerzainebkhlifi/DAO/Entities/Reservation.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class Reservation {
   @Id
   private long idReservation;
-  private Date anneUniversitaire;
+  private Date anneeUniversitaire;
   private Boolean estValide;
 
 
