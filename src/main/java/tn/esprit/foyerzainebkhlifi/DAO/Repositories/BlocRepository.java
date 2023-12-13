e19b53ec3f29a103693ae39e6b0137c98c6404a6
zainebkhlifi1 <zaineb.khlifi@esprit.tn>
2023-12-13 15:48:12 +0100

    keywords

--- src/main/java/tn/esprit/foyerzainebkhlifi/DAO/Repositories/BlocRepository.java
+++ src/main/java/tn/esprit/foyerzainebkhlifi/DAO/Repositories/BlocRepository.java
@@ -4,4 +4,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tn.esprit.foyerzainebkhlifi.DAO.Entities.Bloc;
 
 public interface BlocRepository extends JpaRepository<Bloc,Long> {
+
+    Bloc findByNomBloc(String nomBloc);
+    Bloc getByCapaciteBloc(int capacite);
+    Bloc findByNomBlocIgnoreCase(String nomBloc);
+    Bloc getByCapaciteBlocGreaterThan(int capaciteBloc);
+    Bloc findByNomBlocContains(String nomBloc);
+    Bloc findByNomBlocOrderByNomBloc(String nomBloc);
+
+    
+
+
+
+
+
+
+
+
 }
--- src/main/java/tn/esprit/foyerzainebkhlifi/DAO/Repositories/EtudiantRepository.java
+++ src/main/java/tn/esprit/foyerzainebkhlifi/DAO/Repositories/EtudiantRepository.java
@@ -4,5 +4,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tn.esprit.foyerzainebkhlifi.DAO.Entities.Bloc;
 import tn.esprit.foyerzainebkhlifi.DAO.Entities.Etudiant;
 
+import java.util.List;
+
 public interface EtudiantRepository extends JpaRepository<Etudiant,Long> {
+
+    //select * from Etudiant where cin=
+    List<Etudiant> findByCin(long cin );
+
+    //slect * from Etudiant where nom like
+    List<Etudiant> findByNomETContains(String nomET);
+
+    List<Etudiant> findByNomETContaining(String nomET);
+
+
+
+
+
+
+
+
+
+
+
+
 }
